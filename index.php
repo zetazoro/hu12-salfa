093d3faa684ec0979fe8256485dff9bf3cc4a315
Sebastian <atcseba@gmail.com>
2022-10-19 11:05:41 -0300

    Update recaptura lead

--- index.php
+++ index.php
@@ -17,7 +17,6 @@
       <th scope="col">Linea de Negocio</th>
       <th scope="col">Servicio C4</th>
       <th scope="col">Link</th>
-      <th scope="col">Repositorio</th>
       <th scope="col"></th>
     </tr>
   </thead>
@@ -27,30 +26,42 @@
       <td>HU12</td>
       <td>-</td>
       <td>Tarea</td>
-      <td><a href="activityTask/hu12/index.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link" viewBox="0 0 16 16">
+      <td>
+        <a href="activityTask/hu12/index.php">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link" viewBox="0 0 16 16">
             <path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z"></path>
             <path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z"></path>
-          </svg></a></td>
+          </svg>
+        </a>
+      </td>
     </tr>
     <tr>
       <th scope="row">2</th>
       <td>Lead Agricola</td>
       <td>Agrícola</td>
       <td>Lead</td>
-      <td><a href="Lead/lead-agricola/index.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link" viewBox="0 0 16 16">
+      <td>
+        <a href="Lead/lead-agricola/index.php">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link" viewBox="0 0 16 16">
             <path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z"></path>
             <path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z"></path>
-          </svg></a></td>
+          </svg>
+        </a>
+      </td>
     </tr>
     <tr>
       <th scope="row">3</th>
       <td>Recaptura Lead</td>
       <td>AutyCam</td>
       <td>Lead</td>
-      <td><a href="Lead/recaptura-lead/index.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link" viewBox="0 0 16 16">
+      <td>
+        <a href="Lead/recaptura-lead/index.php">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link" viewBox="0 0 16 16">
             <path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z"></path>
             <path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z"></path>
-          </svg></a></td>
+          </svg>
+        </a>
+      </td>
     </tr>
     <tr>
       <th scope="row">4</th>
@@ -71,10 +82,14 @@
       <td>Repuestos Agrícolas</td>
       <td>Repuestos</td>
       <td>Lead</td>
-      <td><a href="Lead/repuesto-agricola/index.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link" viewBox="0 0 16 16">
+      <td>
+        <a href="Lead/repuesto-agricola/index.php">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link" viewBox="0 0 16 16">
             <path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z"></path>
             <path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z"></path>
-          </svg></a></td>
+          </svg>
+        </a>
+      </td>
     </tr>
   </tbody>
 </table>
